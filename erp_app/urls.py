8dccb30e5fe28b2ca8ccad7a20bad06f39a23790
Rzyczu <70780585+Rzyczu@users.noreply.github.com>
2024-10-23 23:29:11 +0200

    improvments

--- erp_app/urls.py
+++ erp_app/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', views.auth_view, name='auth'),
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
-    path('dashboard/', views.dashboard_view, name='dashboard'),
     path('logout/', views.logout_view, name='logout'),
-
+    path('dashboard/', views.dashboard_view, name='dashboard'),
 ]
