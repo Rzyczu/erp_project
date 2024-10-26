4c0936c93461553d04a3e5cbc7cd747b30390a2f
Rzyczu <70780585+Rzyczu@users.noreply.github.com>
2024-10-26 23:21:03 +0200

    yes

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 # Exit on error
 set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r requirements.txt
+er (pip, poetry, etc.)
+pip install -r # Modify this line as needed for your package managrequirements.txt
 
 # Convert static asset files
 python manage.py collectstatic --no-input
